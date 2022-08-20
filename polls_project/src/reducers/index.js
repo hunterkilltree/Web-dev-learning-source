e009c6d4ae314aaa6451bcd53ddbb885e4adc80e
Trai Le <snguoila@yahoo.com>
2022-08-21 05:56:37 +0700

    Add user

--- polls_project/src/reducers/index.js
+++ polls_project/src/reducers/index.js
@@ -1,6 +1,8 @@
 import { combineReducers } from 'redux'
 import authedUser from './authedUser'
+import users from './user'
 
 export default combineReducers({
-  authedUser
+  authedUser,
+  users
 })
